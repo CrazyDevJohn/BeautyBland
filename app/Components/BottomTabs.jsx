4872b21855ef5475e6f79a3d80ea461be66eedfe
CrazyDevilJohn <7425264@gmail.com>
2024-03-05 12:52:07 +0530

    commited

--- app/Components/BottomTabs.jsx
+++ app/Components/BottomTabs.jsx
@@ -8,7 +8,7 @@ const BottomTabs = ({ activeScreen }) => {
 
   return (
     <View className="absolute bottom-6 w-full px-8">
-      <View className="bg-[#130d2d] rounded-xl px-4 py-6 w-full flex-row items-center justify-around">
+      <View className=" rounded-xl px-4 py-6 w-full flex-row items-center justify-around">
         <TouchableOpacity>
           <FontAwesome name="user" size={32} color={"#5c5576"} />
         </TouchableOpacity>
